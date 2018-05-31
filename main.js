fb39b8d3f6eab5d726c1a0af1e0c60c87abef5f8
Justin Lane <justin.l.lane@gmail.com>
2018-05-30 21:01:57 -0600

    user-data.json
    Window bounds and maximize state is now persistent!

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
 const {app,BrowserWindow} = require('electron');
+const path = require('path');
+const Store = require('./store.js');
+
+const store = new Store({
+  configName: 'user-data',
+  defaults: {
+    windowBounds: {
+      width: 800,
+      height: 600,
+      fullscreen: false
+    }
+  }
+});
 
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+ let {width,height,fullscreen} = store.get('windowBounds');
+
+  win = new BrowserWindow({width,height});
 
   win.loadURL(`file://${__dirname}/dist/anion/index.html`);
-  win.maximize();
-  win.openDevTools();
+
+  if(fullscreen) {
+    win.maximize();
+  }
+
+  //win.openDevTools();
 
   win.on('closed',function() {
     win = null;
-  })
+  });
+
+  win.on('resize',() => {
+    let {width,height} = win.getBounds();
+    store.set('windowBounds',{width,height,fullscreen});
+  });
+
+  win.on('maximize',() => {
+    fullscreen = true;
+    store.set('windowBounds',{width,height,fullscreen});
+  });
+
+  win.on('unmaximize',() => {
+    fullscreen = false;
+    store.set('windowBounds',{width,height,fullscreen});
+  });
 }
 
 app.on('ready',createWindow);
--- src/store.js
+++
@@ -1,3 +0,0 @@
-const electron = require('electron');
-const path = require('path');
-const fs = require('fs');
